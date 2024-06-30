a1e58a06b9773334492ee7fb77b3a2bad8dd30d7
majdhallak <majdhallak90@gmail.com>
2024-06-30 14:56:44 +0100

    custom radix UI theme

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,15 @@
 "use cleint";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, TextArea, TextField, ThemePanel } from "@radix-ui/themes";
 import React from "react";
 
 const NewIssuePage = () => {
   return (
+
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Title"></TextField.Root>
       <TextArea placeholder="Description" />
       <Button>Submit New Issue</Button>
+
     </div>
   );
 };
